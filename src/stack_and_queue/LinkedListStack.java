b46fabbb2551b57b048d90f5cc31bdde8eed0956
chenhorin <chenhorin@163.com>
2018-09-27 14:19:47 +0800

    linkedliststack

---
+++ src/stack_and_queue/LinkedListStack.java
@@ -0,0 +1,69 @@
+package stack_and_queue;
+
+import linkedlist.LinkedList;
+
+public class LinkedListStack<E> implements Stack<E> {
+
+    private LinkedList<E> data;
+
+    private int size;
+
+    public LinkedListStack() {
+        data = new LinkedList();
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        data.addLast(e);
+    }
+
+    @Override
+    public E pop() {
+        return data.removeLast();
+    }
+
+    @Override
+    public E peek() {
+        return data.getLast();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        /*res.append(String.format("ArrayStack : "))不需要再像数组一样要求*/
+        res.append("Stack :\n");
+        res.append("[");
+        /* res.append(data.toString());这是返回数组的具体格式,所以不匹配*/
+        for (int i = 0; i < data.getSize(); i++) {
+            res.append(data.get(i));
+            if (i != data.getSize() - 1)
+                res.append(" , ");
+        }
+        /*res.append("]");还需要加top*/
+        res.append("] top");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack<Integer> listStack = new LinkedListStack<>();
+
+        for (int i = 0; i < 122; i++) {
+            listStack.push(i);
+        }
+        System.out.println(listStack);
+        for (int i = 0; i < 121; i++) {
+            listStack.pop();
+        }
+        System.out.println(listStack);
+    }
+}
