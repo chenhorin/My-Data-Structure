055b72d04fa11d762246ff91069da2d99394d4e4
chenhorin <chenhorin@163.com>
2018-09-28 11:49:33 +0800

    不考虑内存情况和加入dummyhead后的简洁书写

--- src/linkedlist/solutions/ListNode.java
+++ src/linkedlist/solutions/ListNode.java
@@ -12,6 +12,9 @@ public class ListNode {
     int val;
     ListNode next;
 
+    public ListNode() {
+    }
+
     ListNode(int x) {
         val = x;
     }
--- src/linkedlist/solutions/Solutions.java
+++ src/linkedlist/solutions/Solutions.java
@@ -1,6 +1,7 @@
 package linkedlist.solutions;
 
 import javax.sound.midi.Soundbank;
+import java.util.List;
 
 public class Solutions<E> {
 
@@ -26,12 +27,43 @@ public class Solutions<E> {
         }
         return head;
     }
+    public ListNode removeElements2(ListNode head, int e) {
+//        当不需要考虑内存的释放问题时,不需要再额外做一个指针
+        while (head != null && head.val == e) {
+            head = head.next;
+        }
+        if (head == null)
+            return null;
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == e) {
+                prev.next = prev.next.next;
+//                改变的节点依然可能是待删除的节点,所以需要再次循环
+            } else
+                prev = prev.next;
+        }
+        return head;
+    }
+
+    public ListNode removeElements3(ListNode head, int e) {
+        ListNode dummyhead = new ListNode();
+        dummyhead.next = head;
+        ListNode prev = dummyhead;
+        while (prev.next != null) {
+            if (prev.next.val == e) {
+                prev.next = prev.next.next;
+            }else
+                prev = prev.next;
+        }
+        return dummyhead.next;
+    }
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7};
+        int[] arr = {1, 1,2, 2, 2, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7};
         ListNode node = new ListNode(arr);
         System.out.println(node);
-        System.out.println(new Solutions().removeElements(node, 2));
+        System.out.println(new Solutions().removeElements(node, 1));
 
     }
 }
